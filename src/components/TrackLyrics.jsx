11c4138e6e00f12ca3c4e554e27716be655e360b
anisapatel <anisa_patel@live.co.uk>
2020-11-13 16:28:24 +0000

    added basic styling and title

--- src/components/TrackLyrics.jsx
+++ src/components/TrackLyrics.jsx
@@ -13,7 +13,9 @@ class TrackLyrics extends Component {
   render() {
     return (
       <section className="lyrics">
-        <h2 className="lyrics__title">{this.props.title}</h2>
+        <h2 className="lyrics__title">
+          "{this.props.title}" by {this.props.artist}
+        </h2>
         <p className="lyrics__text">{this.state.lyrics}</p>
       </section>
     );
